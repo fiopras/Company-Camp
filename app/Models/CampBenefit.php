5d31f8b4c2ee8ff5c43d8eee3c62fd67c1a0573f
fiopras <fioprasetyo1689@gmail.com>
2023-02-11 02:12:49 +0700

    finish all migrations

---
+++ app/Models/CampBenefit.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\softDeletes;
+
+class CampBenefit extends Model
+{
+    use HasFactory, softDeletes;
+
+    protected $fillable = [
+        'name'
+    ];
+}
